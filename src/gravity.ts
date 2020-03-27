68c550201f0d8ce7a8b8173c8f72ee6c04b9a420
petevdp <pjvanderpol@gmail.com>
2020-03-27 08:49:14 -0700

    fix gravity implementation bug, refactor

--- src/gravity.ts
+++ src/gravity.ts
@@ -1,44 +1,91 @@
 import p5 from "p5";
 
-interface gravitySource {
+export interface gravitySource {
   point: p5.Vector;
   magnitude: number;
 }
 
-export class GravityMover {
-  private velocity: p5.Vector;
+interface gravityMoverInterface {
+  location: p5.Vector;
+  velocity: p5.Vector;
+  energy: (middle: p5.Vector) => number;
+  update: (sources: Array<gravitySource>) => void;
+  draw: (offset: null | p5.Vector) => void;
+}
+
+export class GravityMover implements gravityMoverInterface {
+  private _velocity: p5.Vector;
   constructor(
     private p: p5,
-    public readonly location: p5.Vector,
+    private _location: p5.Vector,
     private radius = 16
   ) {
-    this.velocity = p.createVector(0, 0);
+    this._velocity = p.createVector(0, 0);
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  get velocity() {
+    return this._velocity;
+  }
+
+  energy(middle: p5.Vector) {
+    return p5.Vector.add(middle, this._location).mag();
   }
 
   update(sources: Array<gravitySource>) {
     const acceleration = sources.reduce(
-      (d, s) => p5.Vector.add(d, this.getGravityAccelerationFromPoint(s)),
+      (d, s) => p5.Vector.add(d, this.getGravityAcceleration(s)),
       this.p.createVector(0, 0)
     );
-    this.velocity.add(acceleration.x, acceleration.y);
-    this.location.add(this.velocity.x, this.velocity.y);
+    this._velocity = p5.Vector.add(this._velocity, acceleration);
+    this._location = p5.Vector.add(this._location, this._velocity);
   }
 
-  draw() {
+  draw(offset: null | p5.Vector = null) {
     this.p.stroke(0);
     this.p.fill(175);
 
     const diameter = this.radius * 2;
-    this.p.ellipse(this.location.x, this.location.y, diameter, diameter);
+    let displayPos: p5.Vector;
+    if (offset) {
+      displayPos = p5.Vector.add(offset, this._location);
+    } else {
+      displayPos = this._location;
+    }
+
+    this.p.ellipse(displayPos.x, displayPos.y, diameter, diameter);
   }
 
-  protected getGravityAccelerationFromPoint(source: gravitySource) {
-    const diff = p5.Vector.sub(source.point, this.location);
+  private getGravityAcceleration(source: gravitySource) {
+    const diff = p5.Vector.sub(source.point, this._location);
     const direction = diff.normalize();
     const distance = diff.mag();
 
     const acceleration = source.magnitude * (1 / distance ** 2);
 
-    return p5.Vector.mult(direction, acceleration);
+    return acceleration !== 0 && isFinite(acceleration)
+      ? p5.Vector.mult(direction, acceleration)
+      : direction;
+  }
+}
+
+export class MovingGravitySource extends GravityMover {
+  constructor(
+    public readonly gravity: number,
+    p: p5,
+    location: p5.Vector,
+    radius = 16
+  ) {
+    super(p, location, radius);
+  }
+
+  get source(): gravitySource {
+    return {
+      point: this.location,
+      magnitude: this.gravity
+    };
   }
 }
--- src/nBodyGravityMover.ts
+++ src/nBodyGravityMover.ts
@@ -1,32 +1,47 @@
 import { times } from "lodash-es";
 
 import { sketchFunction } from "./sketch";
-import { GravityMover } from "./gravity";
+import { GravityMover, gravitySource, MovingGravitySource } from "./gravity";
+import p5 from "p5";
 
 const nBodyGravityMovers: sketchFunction = p => {
-  let movers: Array<GravityMover>;
   const gravity = 0.5;
+  let moverSources: Array<MovingGravitySource>;
 
   p.setup = function() {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(255);
 
-    movers = times(15).map(() => {
+    moverSources = times(3).map(() => {
       const location = p.createVector(p.random(p.width), p.random(p.height));
-      return new GravityMover(p, location);
+      return new MovingGravitySource(gravity, p, location);
     });
   };
 
   p.draw = () => {
     p.background(255);
-    const sources = movers.map(m => ({
-      point: m.location,
-      magnitude: gravity
-    }));
-    movers.forEach(m => {
-      m.update(sources);
-      m.draw();
+    const sources = moverSources.map(m => m.source);
+    moverSources.forEach((mover, index) => {
+      mover.update(sources.filter((_, i) => i !== index));
     });
+
+    const posSum = moverSources.reduce(
+      (sum, m) => sum.add(m.location),
+      p.createVector(0, 0)
+    );
+
+    const averagePosition = posSum.div(moverSources.length);
+    const middle = p.createVector(p.width / 2, p.height / 2);
+    const offset = p5.Vector.sub(middle, averagePosition);
+    p.stroke(124);
+    p.fill(124);
+    p.ellipse(middle.x, middle.y, 14, 14);
+
+    moverSources.forEach(m => {
+      m.draw(offset);
+    });
+
+    console.log(moverSources.reduce((s, m) => s + m.energy(middle), 0));
   };
 };
 
