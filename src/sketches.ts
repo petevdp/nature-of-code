40276efd5472dee095b933bb03177d073cba8b65
petevdp <pjvanderpol@gmail.com>
2020-03-27 22:12:41 -0700

    add simple rotation

--- src/sketches.ts
+++ src/sketches.ts
@@ -146,3 +146,28 @@ sketches.randomMovers = p => {
     });
   };
 };
+
+sketches.simpleRotation = p => {
+  let angle = 0;
+  let aVelocity = 0;
+  let aAcceleration = 0.001;
+
+  p.setup = function() {
+    p.createCanvas(640, 360);
+  };
+
+  p.draw = function() {
+    p.background(255);
+
+    p.fill(175);
+    p.stroke(0);
+    p.rectMode(p.CENTER);
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(angle);
+    p.line(-50, 0, 50, 0);
+    p.ellipse(50, 0, 8, 8);
+
+    aVelocity += aAcceleration;
+    angle += aVelocity;
+  };
+};
