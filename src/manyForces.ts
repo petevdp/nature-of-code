8035868f1e39abbe644b1414175432f21c8c574e
petevdp <pjvanderpol@gmail.com>
2020-03-27 21:50:57 -0700

    add liquid to manyForces

--- src/manyForces.ts
+++ src/manyForces.ts
@@ -6,22 +6,27 @@ import { Mover } from "./mover";
 export { manyForces };
 
 const manyForces: sketchFunction = p => {
-  let wind: p5.Vector;
-  const coefficientOfFriction = 0.2;
-  const normal = 1;
-  const frictionMagnitude = coefficientOfFriction * normal;
+  const wind = p.createVector(0.8, 0);
+  const startingPosition = p.createVector(16 * 5, 16 * 5);
 
   let gravityAcceleration: p5.Vector;
   let movers: Array<Mover>;
+  let liquid: Liquid;
 
   p.setup = function() {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(255);
 
-    const startingPosition = p.createVector(16 * 5, 16 * 5);
-
-    wind = p.createVector(0.8, 0);
     gravityAcceleration = p.createVector(0, 1);
+
+    {
+      const size = p.createVector(400, 400);
+      const position = p.createVector(
+        p.width / 2 - size.x / 2,
+        p.height / 2 - size.x / 2
+      );
+      liquid = new Liquid(p, position, size, 0.01);
+    }
 
     movers = times(100).map(() => {
       const position = startingPosition.copy();
@@ -35,24 +40,59 @@ const manyForces: sketchFunction = p => {
 
   p.draw = function() {
     p.background(255);
+    liquid.draw();
     movers.forEach((mover, index) => {
       const gravityForce = p5.Vector.mult(gravityAcceleration, mover.mass);
-      const frictionForce = mover.velocity
-        .copy()
-        .normalize()
-        .mult(-1)
-        .mult(frictionMagnitude);
+      let liquidFrictionForce = liquid.isInLiquid(mover)
+        ? liquid.frictionForMover(mover)
+        : p.createVector(0, 0);
 
-      const netForce = [wind, gravityForce, frictionForce].reduce(
+      const netForce = [wind, gravityForce, liquidFrictionForce].reduce(
         (acc, f) => p5.Vector.add(acc, f),
         p.createVector(0, 0)
       );
       mover.update(netForce);
+
       mover.draw();
     });
   };
 };
 
 class Liquid {
-  constructor(private position: p5.Vector, private dimensions: p5.Vector) {}
+  constructor(
+    private p: p5,
+    private position: p5.Vector,
+    private dimensions: p5.Vector,
+    private frictionCoefficient: number
+  ) {}
+
+  draw() {
+    this.p.noStroke();
+    this.p.fill(175);
+    this.p.rect(
+      this.position.x,
+      this.position.y,
+      this.dimensions.x,
+      this.dimensions.y
+    );
+  }
+
+  isInLiquid(mover: Mover) {
+    return (
+      mover.position.x > this.position.x &&
+      mover.position.y > this.position.y &&
+      mover.position.x < this.position.x + this.dimensions.x &&
+      mover.position.y < this.position.y + this.dimensions.y
+    );
+  }
+
+  frictionForMover(mover: Mover) {
+    const dragMagnitude = this.frictionCoefficient * mover.velocity.mag() ** 2;
+
+    return mover.velocity
+      .copy()
+      .mult(-1)
+      .normalize()
+      .mult(dragMagnitude);
+  }
 }
